e9dd7615804f867e7d6ec960a765823580ddbaf4
Mubarak Awal <awalmubarak6@gmail.com>
2021-03-27 15:22:26 +0000

    wire up userForm and user list component to use main state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import UserForm from './components/UserForm';
+import UserList from './components/UserList';
+import { useState } from 'react';
 
 function App() {
+  //we have a main state that holds all our users
+  //this state will be modified by our userform component anytime
+  //we want to add a new user
+
+  //This state will also be used by our userlist component to display
+  //all the users we have in our application
+  const [users, setUsers] = useState([]);
+
+  //this is a helper function that adds a new user to our users state
+  //we later pass this function to userform component to be called anytime
+  //we want to create a new user
+  function addUser(user) {
+    setUsers([...users, user]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          {/* we pass the addUser function to userform as a props to be called whenever
+          we want to create a new user */}
+          <UserForm addUser={addUser} />
+        </div>
+        <div className="col-md-6">
+          {/* we pass the users down to userlist component to be displayed */}
+          <UserList allUsers={users} />
+        </div>
+      </div>
     </div>
   );
 }
