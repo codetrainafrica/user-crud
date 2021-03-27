5795b8840546bc2f7581fc361293646f2c5b5f9f
Mubarak Awal <awalmubarak6@gmail.com>
2021-03-27 15:21:23 +0000

    create user time component

---
+++ src/components/UserItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+//this component represents a single user that's displayed in the
+//userlist component
+function UserItem(props) {
+  //we accept the user a prop and display the user's information
+  return (
+    <div className="row">
+      <div className="col-md-3">{props.user.username}</div>
+      <div className="col-md-3">{props.user.email}</div>
+      <div className="col-md-2">{props.user.country}</div>
+      <div className="col-md-4">
+        <button>Update</button>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default UserItem;
