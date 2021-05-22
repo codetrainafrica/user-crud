8b0060d5faa319e4da12abc5b9db627397f9e5bc
Mubarak Awal <awalmubarak6@gmail.com>
2021-05-22 13:18:45 +0000

    setup redux

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,11 @@
+export default function (state = {}, action) {
+  switch (action.type) {
+    case 'ADD_USER':
+    //code to add user to the state
+    case 'DELETE_USER':
+    //code to delete user
+
+    default:
+      return state;
+  }
+}
