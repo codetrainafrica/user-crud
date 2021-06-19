81389ad5725f5f662e0e2440a99300af96a71eed
Mubarak Awal <awalmubarak6@gmail.com>
2021-06-19 14:10:48 +0000

    handle action to add all users in reducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -2,6 +2,9 @@ const initalState = { users: [] };
 
 export default function (state = initalState, action) {
   switch (action.type) {
+    case 'ADD_ALL_USERS':
+      let users = action.payload;
+      return { users: users };
     case 'ADD_USER':
       let newUsers = [...state.users, action.payload];
       return { users: newUsers };
