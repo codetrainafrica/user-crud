35ec6e0847458225fda84d52a2b8381164fb1b97
Mubarak Awal <awalmubarak6@gmail.com>
2021-06-26 14:36:42 +0000

    refactor .then .catch to use async await

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -27,55 +27,34 @@ export function getAllUsers() {
 }
 
 export function addUserAction(user) {
-  return (dispatch, state, { getFirestore }) => {
+  return async (dispatch, state, { getFirestore }) => {
     const db = getFirestore();
-    db.collection('users')
-      .add(user)
-      .then(() => {
-        //send the data to the store
-        // dispatch({
-        //   type: 'ADD_USER',
-        //   payload: user,
-        // });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try {
+      await db.collection('users').add(user);
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
 
 export function deleteUserAction(id) {
-  return (dispatch, state, { getFirestore }) => {
+  return async (dispatch, state, { getFirestore }) => {
     let db = getFirestore();
-    db.collection('users')
-      .doc(id)
-      .delete()
-      .then(() => {
-        // dispatch({
-        //   type: 'DELETE_USER',
-        //   payload: id,
-        // });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try {
+      await db.collection('users').doc(id).delete();
+    } catch (err) {
+      console.log(err);
+    }
   };
 }
 
 export function updateUserAction(id, updatedUser) {
-  return (dispatch, state, { getFirestore }) => {
+  return async (dispatch, state, { getFirestore }) => {
     let db = getFirestore();
-    db.collection('users')
-      .doc(id)
-      .update(updatedUser)
-      .then(() => {
-        // dispatch({
-        //   type: 'UPDATE_USER',
-        //   payload: { id: id, updatedUserInfo: updatedUser },
-        // });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try {
+      await db.collection('users').doc(id).update(updatedUser);
+    } catch (err) {
+      console.log(err);
+    }
   };
 }
