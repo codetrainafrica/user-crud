7770b065b30d4c883b1e474a381d9cea772dddab
Mubarak Awal <awalmubarak6@gmail.com>
2021-07-10 15:18:05 +0000

    use protected route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import Dashboard from './pages/dashboard';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import { BrowserRouter, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import Settings from './pages/Settings';
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
-        <Route path="/" exact component={Dashboard} />
+        <ProtectedRoute path="/" exact component={Dashboard} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <ProtectedRoute path="/settings" component={Settings} />
       </BrowserRouter>
     </div>
   );
